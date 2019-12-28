d69caf502f781854d29622a9e9e1fc9e8c169396
charyung <charmaineyung@yahoo.com.hk>
2019-12-28 02:18:22 -0500

    more tailwind setup

--- tailwind.js
+++ tailwind.js
@@ -425,7 +425,7 @@ module.exports = {
     alignSelf: ['responsive'],
     appearance: ['responsive'],
     backgroundAttachment: ['responsive'],
-    backgroundColor: ['responsive', 'hover', 'focus'],
+    backgroundColor: ['responsive', 'hover', 'focus', 'active'],
     backgroundPosition: ['responsive'],
     backgroundRepeat: ['responsive'],
     backgroundSize: ['responsive'],
