f4a0bf71578d75dac0e8d4ea98997d89bd63530e
charyung <charmaineyung@yahoo.com.hk>
2020-01-02 22:14:26 -0500

    custom time control

--- src/TimeController/TimeController.js
+++ src/TimeController/TimeController.js
@@ -12,11 +12,12 @@ import {
 // CSS
 import classnames from 'classnames';
 import classes from './TimeController.module.scss';
+const inputStyle = 'px-4 py-1 my-1 rounded-full';
 
 const Button = ({ children, ...props }) => (
     <button
         className={classnames(
-            'px-4 py-1 my-1 rounded-full',
+            inputStyle,
             { 'bg-red-700 active:bg-red-800 text-white': props.colour === 'red' },
             { 'bg-gray-500 active:bg-gray-600': !props.colour }
         )}
@@ -31,6 +32,9 @@ const TimeController = ({ modifyTime, resetToPresent, ...props }) => {
     const [addTime, setAddTime] = useState(true);
     const modifier = addTime ? 1 : -1;
     const date = new Date(props.time);
+
+    const [customTime, setCustomTime] = useState(0);
+    const [customUnit, setCustomUnit] = useState(0);
 
     return (
         <div className="inline-block p-4 m-4 rounded-lg bg-gray-400 text-gray-800">
@@ -108,7 +112,47 @@ const TimeController = ({ modifyTime, resetToPresent, ...props }) => {
                         </Button>
                     </div>
                 </div>
-                <div className="p-2 m-2 rounded-lg bg-gray-300">Custom</div>
+                <div className="p-2 m-2 rounded-lg bg-gray-300">
+                    Custom
+                    <div className="flex flex-col">
+                        <input
+                            className={inputStyle}
+                            placeholder="Duration"
+                            type="number"
+                            min="0"
+                            required
+                            value={customTime}
+                            onChange={e => {
+                                if (e.target.checkValidity()) {
+                                    setCustomTime(parseInt(e.target.value));
+                                }
+                            }}
+                        ></input>
+                        <div className={classes.Dropdown}>
+                            <select
+                                className={classnames(
+                                    inputStyle,
+                                    'appearance-none',
+                                    'w-full'
+                                )}
+                                value={customUnit}
+                                onChange={e => {
+                                    if (e.target.checkValidity()) {
+                                        setCustomUnit(parseInt(e.target.value));
+                                    }
+                                }}
+                            >
+                                <option value="1">seconds</option>
+                                <option value="60">minutes</option>
+                                <option value="3600">hours</option>
+                                <option value="84000">days</option>
+                            </select>
+                        </div>
+                        <Button onClick={() => {
+                            modifyTime(customTime * customUnit * modifier);
+                        }}>+</Button>
+                    </div>
+                </div>
             </div>
         </div>
     );
